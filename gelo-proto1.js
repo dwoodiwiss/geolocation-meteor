21ca48beac1e68204a2697a153b0e393236a01fe
Darren Woodiwiss <darrenwoodiwiss@gmail.com>
2015-07-05 03:42:08 +0100

    add google map, get lat long from last position saved from mongodb store

--- gelo-proto1.js
+++ gelo-proto1.js
@@ -25,6 +25,7 @@ if (Meteor.isClient) {
   });
 
   Meteor.startup(function() {
+    GoogleMaps.load();
     Meteor.setInterval(function() {
       navigator.geolocation.getCurrentPosition(function(position) {
         if (Positions.find().count() > 100) {
@@ -51,6 +52,39 @@ if (Meteor.isClient) {
       });
     }, 2000);
   });
+
+  // Google Maps Related
+  Template.body.helpers({
+
+    exampleMapOptions: function() {
+      var tot = Positions.find().count();
+      var lat = Positions.findOne().latitude;
+      var lon = Positions.findOne().longitude;
+
+      // Make sure the maps API has loaded
+      if (GoogleMaps.loaded()) {
+        // Map initialization options
+        return {
+          center: new google.maps.LatLng(lat, lon),
+          // center: new google.maps.LatLng(-37.8136, 144.9631),
+          zoom: 18
+        };
+      }
+    }
+  });
+
+  Template.body.onCreated(function() {
+    // We can use the `ready` callback to interact with the map API once the map is ready.
+    GoogleMaps.ready('exampleMap', function(map) {
+      // Add a marker to the map once it's ready
+      var marker = new google.maps.Marker({
+        position: map.options.center,
+        map: map.instance
+      });
+    });
+  });
+
+
 }
 
 if (Meteor.isServer) {
